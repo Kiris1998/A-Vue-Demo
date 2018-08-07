50eb329b4760d18506632d828094a3ffb525a92c
Kiris_1013 <768049133@qq.com>
2018-08-07 18:18:08 +0800

    add element-ui

--- src/components/pages/Pos.vue
+++ src/components/pages/Pos.vue
@@ -1,5 +1,29 @@
 <template>
     <div class="pos">
-        Hello Pos!
+        <el-row>
+            <el-col :span='7' class="order" :style="{height:orderHeight}">订单</el-col>
+            <el-col :span='7'>商品</el-col>
+        </el-row>
     </div>
 </template>
+
+<script>
+export default{
+  name: 'Pos',
+  data () {
+    return {
+      orderHeight: ''
+    }
+  },
+  mounted: function () {
+    this.orderHeight = document.body.clientHeight + 'px'
+  }
+}
+</script>
+
+<style scoped>
+    .order{
+        background: #f9fafc;
+        border-right: solid 1px #C0CCDA;
+    }
+</style>
